6863b54d24625ad18d7bbe016f7ad4e31031a452
NikWhit <135679785+NikWhit@users.noreply.github.com>
2023-12-19 03:38:16 -0700

    client side files check, going through utilities

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,18 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
+      <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
+      </ApolloProvider>
     </>
   );
 }
