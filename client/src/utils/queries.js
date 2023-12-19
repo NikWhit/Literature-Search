a251b6a45e9935ac7f0fb4b8b7c0a3a6edba1780
NikWhit <135679785+NikWhit@users.noreply.github.com>
2023-12-19 03:46:36 -0700

    finishing up the utils

---
+++ client/src/utils/queries.js
@@ -0,0 +1,17 @@
+import { gql } from '@apollo/client';
+
+export const GET_USER = gql`
+    query GetSingleUser($username: String) {
+    getSingleUser(username: $username) {
+        _id
+        username
+        savedBooks {
+            authors
+            bookId
+            description
+            image
+            link
+            title
+        }
+    }
+    }`;
