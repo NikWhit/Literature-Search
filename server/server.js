05163f27d3155b458519e28b53b14bb45c4ce042
NikWhit <135679785+NikWhit@users.noreply.github.com>
2024-02-22 21:00:27 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,7 +28,9 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
-
+ app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
